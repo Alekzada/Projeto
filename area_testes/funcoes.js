e5c561fb76759d8bb281fdf53759993c3546d68d
Alekzada <alexismolica.6a@hotmail.com>
2022-05-30 21:49:16 -0300

    site

---
+++ area_testes/funcoes.js
@@ -0,0 +1,28 @@
+var nome = 'alexis' //string
+var idade = 16     //int
+var semdefinicao   //undefined
+var nula = null    //objeto (!?)
+
+const nota = 0                //int
+const sobrenome = 'vinícius'  //string
+const naodefinida             //undefined
+
+let x = 3                     //int
+let jogo = 'Habbo'            //string
+let icia                      //undefined
+
+// Operadores Lógicos
+// = (Atribuição)
+// == (Igualdade)
+// === (Igualdade Estrita)
+// && (And)
+// || (or)
+// ! (Not)
+
+x = y                         //Atribui y a x
+var var1 = 3                  //Atribui 3 a variável var1
+3 == var1                     //true
+'3' == var1                   //true
+3 == '3'                      //true
+3 ==== var1                   //true
+3 ==== '3'                    //false
