0fdf65b70819dbf1408b87c40fbaf3fc10bd6e93
Alekzada <alexismolica.6a@hotmail.com>
2022-05-31 20:48:46 -0300

    site

--- area_testes/js/testes_js4.js
+++ area_testes/js/testes_js4.js
@@ -3,9 +3,16 @@ let dia_semana = data_hora.getDay();
 switch (dia_semana) {
     case 0: alert('Domingo de descanso merecido');
         break;
+    case 1: alert('Dia de Trabalhar!');
+        break;
+    case 2: alert('Calma, ainda é terça!');
+        break;
+    case 3: alert('Estamos quase!');
+        break;
+    case 4: alert('Não chegou ainda');
+        break;
     case 5: alert('Chegou a sexta-feira, isso é legal!');
         break;
     case 6: alert('Maravilha, sábado chegou!!');
         break;
-    default: alert('Que semana longa!!');
 }
